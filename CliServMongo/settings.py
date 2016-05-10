24c9073feda3747bef5d2d1c633875207c5c2c12
Juan Carlos <juancagb.17@gmail.com>
2016-05-10 09:12:38 -0500

    Django, forget your databases

--- CliServMongo/settings.py
+++ CliServMongo/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = 'CliServMongo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+import mongoengine
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': ''
     }
 }
 
